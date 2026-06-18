a82c368151bc344422e040833909e859be20ff28
Noa-GH <fordnt17@gmail.com>
2026-06-18 14:09:29 -0400

    chore: attempt at fixing routing for strict error of MIME types for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-  base: '/UchiNihon-frontend',
+  base: '/UchiNihon-frontend/',
   plugins: [react()],
   resolve: {
     alias: {
